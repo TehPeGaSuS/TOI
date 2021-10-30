d230089e51c9099ab16db63b17b7f94af9717a3a
PeGaSuS <droider.pc@gmail.com>
2021-10-30 23:02:16 +0200

    Update purge_plaintext.sh

--- TheLounge/purge_plaintext.sh
+++ TheLounge/purge_plaintext.sh
@@ -3,7 +3,7 @@
 # Path to sqlite logs (usually you don't need to edit this)
 logs=$HOME/.thelounge/logs
 
-# How many days to keep of logs?
+# How many lines to keep of plaintext logs?
 max_logs_lines=1000
 
 # Path to the uploads folder (usually you don't need to edit this)
@@ -11,6 +11,12 @@ uploads=$HOME/.thelounge/uploads
 
 # How many days to keep the files
 max_upload_days=7
+
+###############
+# DON'T TOUCH ANYTHING BELOW UNLESS YOU KNOW WHAT YOU ARE DOING
+###############
+# If you touch the code below and then complain the script "suddenly stopped working" I'll touch you at night. (THANKS thommey)
+###############
 
 # Cleaning up the plaintext logs
 find "$logs" -name "*.log" -print0 | while read -rd $'\0' file

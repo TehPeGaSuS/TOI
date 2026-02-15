750e6200eb97ec83bfec3df2b9ca8bd14d27e30d
TehPeGaSuS <25697531+TehPeGaSuS@users.noreply.github.com>
2026-02-15 17:57:14 +0100

    Bump version from 1.05 to 1.06

--- Blacklist/__init__.py
+++ Blacklist/__init__.py
@@ -36,7 +36,7 @@ import supybot
 from supybot import world
 
 # Use this for the version of this plugin.
-__version__ = "1.05"
+__version__ = "1.06"
 
 # XXX Replace this with an appropriate author or supybot.Author instance.
 __author__ = supybot.authors.unknown
